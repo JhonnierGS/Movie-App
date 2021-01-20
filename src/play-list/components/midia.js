90abda9ac3becdc81d25ff45b8c2095766301fa2
JhonnierGS <gs.170590@gmail.com>
2021-01-20 16:16:33 -0500

    agregando componentes play-list, pages, icons, categories. Se agrega tambien una carpeta images a la carpeta public

---
+++ src/play-list/components/midia.js
@@ -0,0 +1,45 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import './midia.css';
+
+
+class Media extends PureComponent{
+    state = {
+        author: 'jonathan gonzalez'
+    }
+
+    handleClick = (event) =>{
+        this.setState({
+            author:'Ricardo Celis',
+        })
+    }
+
+    render(){
+        const style ={
+            container:{
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red',
+            }
+        }
+        return(
+            <div className='Media' onClick={this.handleClick}>
+                <div className='Media-cover'>
+                    <img src={this.props.cover} alt='foto' width={260} height={160} className='Media-image'></img>
+                    <h3 className='Media-title'>{this.props.title}</h3>
+                    <p className='media-author'>{this.props.author}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+Media.propType = {
+    cover: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio']),
+}
+
+export default Media;
